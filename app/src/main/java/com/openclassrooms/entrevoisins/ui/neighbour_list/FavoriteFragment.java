6840534a769eba8aee025f2cb8201b8e847afcc3
Vincent <dubeaurepaire.v@gmail.com>
2021-07-07 12:15:18 +0200

    + add fragment voisins marqués comme favoris

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavoriteFragment.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavoriteFragment.java
@@ -1,44 +1,39 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.events.DeleteNeighbourEvent;
-import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.service.NeighbourApiService;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
-import java.util.List;
-
-//public class DetailNeighbourFragment extends ListFragment {
-public class DetailNeighbourFragment extends Fragment {
+public class FavoriteFragment extends Fragment {
 
     private NeighbourApiService mApiService;
+
+    public static final String sFAVORITE_NAME = "FAVORITE_NAME";
     //private List<Neighbour> mNeighbours
     private String msg;
     private RecyclerView mRecyclerView;
     /**
      * Create and return a new instance
-     * @return @{@link DetailNeighbourFragment}
+     * @return @{@link FavoriteFragment}
      */
-    public static DetailNeighbourFragment newInstance(final String msg) {
-        DetailNeighbourFragment fragment = new DetailNeighbourFragment();
+    public static FavoriteFragment newInstance(final String name) {
+        FavoriteFragment fragment = new FavoriteFragment();
+        Bundle bd1 = new Bundle();
+        bd1.putString(sFAVORITE_NAME, name);
         return fragment;
     }
 
@@ -46,15 +41,19 @@ public class DetailNeighbourFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mApiService = DI.getNeighbourApiService();
+        if (getArguments() != null) {
+            msg = getArguments().getString(sFAVORITE_NAME);
+            //System.out.println(" detail N fra  : " + msg.toString());
+        }
         //getFragmentManager().beginTransaction().add(R.id.main_content, new PlaceholderFragment()).commit();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup VPviewGroup,
                              Bundle savedInstanceState) {
         //View view = inflater.inflate(R.layout.fragment_detail_neighbour, container, false);
         //Context context = view.getContext();
-        View view = inflater.inflate(R.layout.activity_detail_neighbour, container,false);
+        View view = inflater.inflate(R.layout.activity_list_neighbour, VPviewGroup ,false);
         //mRecyclerView = (RecyclerView) view;
         //mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
         //mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
