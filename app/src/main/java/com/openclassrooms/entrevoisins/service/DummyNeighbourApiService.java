d1d82692a94abaa84eb19fc1fb66762902bbe6d0
Vincent <dubeaurepaire.v@gmail.com>
2021-07-07 11:52:47 +0200

    ajout+  addFavoriteNeighbour deleteFavoriteNeighbour getFavoriteNeighbours lecture liste "favorites" si le booléen est true, puis renvoie la liste "favorites"

--- app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
@@ -2,6 +2,7 @@ package com.openclassrooms.entrevoisins.service;
 
 import com.openclassrooms.entrevoisins.model.Neighbour;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +11,6 @@ import java.util.List;
 public class DummyNeighbourApiService implements  NeighbourApiService {
 
     private List<Neighbour> neighbours = DummyNeighbourGenerator.generateNeighbours();
-
 
     /**
      * {@inheritDoc}
@@ -36,4 +36,34 @@ public class DummyNeighbourApiService implements  NeighbourApiService {
     public void createNeighbour(Neighbour neighbour) {
         neighbours.add(neighbour);
     }
+
+    /**
+     *  ajoute le neighbour en cours de lecture à la liste "favorites" si le booléen est true), puis qui renvoie la liste "favorites" en sortie de boucle:
+     * @param neighbour
+     */
+    @Override
+    public void addFavoriteNeighbour(Neighbour neighbour){
+    neighbour.setFavorite(true);
+    }
+
+    @Override
+    public void deleteFavoriteNeighbour(Neighbour neighbour){
+        neighbour.setFavorite(true);
+    }
+
+    /**
+     * lecture liste "favorites" si le booléen est true, puis renvoie la liste "favorites" en sortie de boucle:
+     */
+    @Override
+    public List<Neighbour> getFavoriteNeighbours(){
+        List <Neighbour> mFavorites = new ArrayList<>();
+        for (int i=0; i< neighbours.size();i++){
+            Neighbour neighbour = neighbours.get(i);
+            if (neighbour.isFavorite()){
+                mFavorites.add(neighbour);
+            }
+        }
+        return mFavorites;
+    }
+
 }
