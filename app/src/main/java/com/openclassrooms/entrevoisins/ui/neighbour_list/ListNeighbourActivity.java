f62025a1d70ef9318540a03d24d288961f527b80
Vincent <dubeaurepaire.v@gmail.com>
2021-07-09 15:17:32 +0200

    + Favorit TabItem click

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
@@ -1,17 +1,23 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.os.Bundle;
+import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TabWidget;
+import android.widget.Toast;
 
 import com.openclassrooms.entrevoisins.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Optional;
 
 public class ListNeighbourActivity extends AppCompatActivity {
 
@@ -20,10 +26,14 @@ public class ListNeighbourActivity extends AppCompatActivity {
     TabLayout mTabLayout;
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
-    @BindView(R.id.container)
+    @BindView(R.id.VP)
     ViewPager mViewPager;
+    //@BindView(R.id.tabItem2)
+    //TabItem mTabItem2;
 
     ListNeighbourPagerAdapter mPagerAdapter;
+    FavoritePagerAdapter mFavAdapter;
+    //private boolean favorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +47,50 @@ public class ListNeighbourActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
         mTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
+        //mTabLayout.addOnTabSelectedListener(new View.OnClickListener(mViewPager));
+
+        FavorisTabItem();
+
+        //mViewPager.setAdapter(mFavAdapter);
+        //mViewPager.addOnAdapterChangeListener();
     }
 
-    @OnClick(R.id.add_neighbour)
+    /*@OnClick(R.id.add_neighbour)
     void addNeighbour() {
         AddNeighbourActivity.navigate(this);
-    }
-
-    /**
-     * Select item
-     */
-    /**@OnClick(R.id.item_list_name)
-    void vuNeighbour() { //DetailNeighbourActivity.DISPLAY_SERVICE;
     }*/
 
+    //@Optional
+    /**@OnClick(R.id.tabItem2)
+    void Favorites() {
+        System.out.println(" put N fav  : ");
+        Toast.makeText(ListNeighbourActivity.this, "Homme", Toast.LENGTH_SHORT).show();
+    }*/
 
+    /**
+     * Ecoute du bouton tabitem
+     * -
+     * - s'il y a bien eu click :
+     * - affichage de l'onglet (favoris)
+     * @param tbFav
+     * @param cls
+     */
+    private void FavorisTabClick(TabItem tbFav, final Class cls) {
+        //((TabItem) findViewById(R.id.tabItem2)).setOnClickListener(new Button.OnClickListener() {
+        Toast.makeText(ListNeighbourActivity.this, "Tab 1", Toast.LENGTH_LONG).show();
+        /**tbFav.setOnClickListener(new ImageButton.OnClickListener() {
+        //tbFav.setOnClickListener(new TabItem().callOnClick() {
+        @Override
+        public void onClick(View v) {
+                Toast.makeText(ListNeighbourActivity.this, "Favoris ", Toast.LENGTH_SHORT).show();
+            }
+        }
+         );*/
+    }
 
+    public void FavorisTabItem(){
+        TabItem tbFav = (TabItem) findViewById(R.id.tabItem2);
+        //btBackAccueil.setImageResource(R.drawable.back);
+        FavorisTabClick(tbFav, ListNeighbourActivity.class);
+            }
 }
