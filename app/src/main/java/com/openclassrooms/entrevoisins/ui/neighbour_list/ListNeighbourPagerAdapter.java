a4f8761392490fd6249d10df26b28f5b52184afd
Vincent <dubeaurepaire.v@gmail.com>
2021-07-27 15:16:46 +0200

    méthode getPageTitle( ) conçue pour retourner le titre de chacune des pages d'un ViewPage

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
@@ -7,6 +7,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
 
+    private String[] tab;
+
     public ListNeighbourPagerAdapter(FragmentManager fm) {
         super(fm);
     }
@@ -18,7 +20,15 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      */
     @Override
     public Fragment getItem(int position) {
-        return NeighbourFragment.newInstance();
+        switch (position) {
+            case 0: //Page number 1
+                return NeighbourFragment.newInstance();
+            case 1: //Page number 2
+                return FavoriteFragment.newInstance();
+            default:
+                return null;
+        }
+        //return NeighbourFragment.newInstance();
     }
 
     /**
@@ -27,6 +37,24 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      */
     @Override
     public int getCount() {
-        return 1;
+        return (2);
+        //return 1;
     }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        //return tab[position];
+        switch (position){
+            case 0: //Page number 1
+                return "voisins";
+            case 1: //Page number 2
+                return "fav";
+            //case 2: //Page number 3
+            //    return "Paramètre";
+            default:
+                return null;
+        }
+
+    }
+
 }
